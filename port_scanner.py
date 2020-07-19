2e589a6f63fab4596e0f82f7f64890070cf5897e
jpie726 <53590846+jpie726@users.noreply.github.com>
2020-07-19 15:21:11 -0500

    a

--- port_scanner.py
+++ port_scanner.py
@@ -2,20 +2,23 @@ from socket import *
 from stringcolor import *
 from time import *
 
-print(cs("   ___              __       _       _ _   \n  / __\___  _ __   / _\_ __ | | ___ (_) |_ \n / /  / _ \| '_ \  \ \| '_ \| |/ _ \| | __|\n/ /__| (_) | | | |__\ \ |_) | | (_) | | |_ \n\____/\___/|_| |_(_)__/ .__/|_|\___/|_|\__|\n                      |_|                  \n", "#FFFF33"))
-print(cs("1. I have the ip, stfu nurd\n2. I have the hostname, go away\n3. What in the god forsaken hell is ip or hostname?!?!?!\n\npress q or quit to exit", "#FF0000"))
+def scan(ip):
+	new = "yes"
+def main():
 
-action = input(cs(">", "#169900"))
-ip = ""
-url = ""
-con = ""
+	print(cs("   ___              __       _       _ _   \n  / __\___  _ __   / _\_ __ | | ___ (_) |_ \n / /  / _ \| '_ \  \ \| '_ \| |/ _ \| | __|\n/ /__| (_) | | | |__\ \ |_) | | (_) | | |_ \n\____/\___/|_| |_(_)__/ .__/|_|\___/|_|\__|\n                      |_|                  \n", "#FFFF33"))
+	print(cs("1. I have the ip, stfu nurd\n2. I have the hostname, go away\n3. What in the god forsaken hell is ip or hostname?!?!?!\n\npress q or quit to exit", "#FF0000"))
 
-error = "Invalid operation, please try again" #169900
+	action = input(cs(">", "#169900"))
+	ip = ""
+	url = ""
+	cont = "true"
+	error = "Invalid operation, please try again" #169900
 
-def idk():
 	if action == "1":
 		print(cs("Input ip", "#7F1515"))
 		ip = input(cs(">", "#169900"))
+		scan(ip)
 	elif action == "2":
 		print(cs("Enter url", "#7F1515"))
 		url = input(cs(">", "#169900"))
@@ -29,18 +32,24 @@ def idk():
 			sleep(10)
 			quit()
 		else:
-			print("Ip address is " + ip + " from " + hostname)
+			print("Ip address is " + ip + " from " + url)
 	elif action == "3":
-		print("Shut the hell up retard, stop faking stupidity. Oh wait, you're too stupid to even think of faking.")
+		print("Shut the hell up retard, stop faking stupidity. Oh wait, you're too much of a demented ass cockroach to even think of faking.")
 		sleep(1)
 		print("\nhttp://tiny.cc/IPvsHostname")
-	elif action == "debug":
+	elif action == "dbg":
 		print("What error number did you get?")
-		error = input(cs(">", "#169900"))
-		if error == "696969":
+		dumbass = input(cs(">", "#169900"))
+		if dumbass == "696969":
 			print("The website you entered did not translate to an ip address or pointed to yourself")
 	elif action == "q" or "quit":
 		quit()
 
-while con == "true":
-	idk()
+	sleep(1)
+	print("Scan again? y or yes/n or no")
+	if again == "y" or "yes":
+		main()
+	elif again == "n" or "no":
+		action = "no"
+
+main()
